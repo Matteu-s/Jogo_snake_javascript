9b82b5e07d0130c3fb141345975601c65c1217b5
Mateus <mateeussantoos@hotmail.com>
2020-06-21 20:10:01 -0300

    criando a snake

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d"); //trata o arquivo como 2d
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 //funcao que desenha e define canvas
 function criarBG() {
@@ -9,4 +14,12 @@ function criarBG() {
 
 }
 
+function criarSnake(){
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarSnake();
